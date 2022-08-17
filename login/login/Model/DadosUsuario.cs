ec4bdb94094f9e122b1a51dddb2578d6d14c04d9
Usuario <Usuario@CPD02-M12>
2022-08-17 14:54:51 -0300

    Adicionar arquivos de projeto.

---
+++ login/login/Model/DadosUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace login.Model
+{
+    public partial class DadosUsuario : Component
+    {
+        public DadosUsuario()
+        {
+            InitializeComponent();
+        }
+
+        public DadosUsuario(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
